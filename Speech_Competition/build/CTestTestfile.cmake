2b0de9e6748c42770d47de331be372de5881c5aa
Heathcliff <1329550246@qq.com>
2024-06-09 16:47:40 +0800

    add .gitignore

--- Speech_Competition/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Documents/C++/MyProjects/Speech_Competition
-# Build directory: F:/Documents/C++/MyProjects/Speech_Competition/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("bin")
