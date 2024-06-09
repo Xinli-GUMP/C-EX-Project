3842bb7d776c9acab93c96c45ea848b3d0baabcc
Heathcliff <1329550246@qq.com>
2024-06-09 17:11:35 +0800

    reset

--- Computer_room_booking_system/build/CMakeFiles/ComputerRoom_booking.dir/src/tercher.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/ComputerRoom_booking.dir/src/tercher.cpp.obj: \
- F:\Documents\C++\MyProjects\Computer_room_booking_system\src\tercher.cpp
--- Computer_room_booking_system/build/bin/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Documents/C++/MyProjects/Computer_room_booking_system/test
-# Build directory: F:/Documents/C++/MyProjects/Computer_room_booking_system/build/bin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[myTest]=] "F:/Documents/C++/MyProjects/Computer_room_booking_system/build/bin/testExecutable.exe")
-set_tests_properties([=[myTest]=] PROPERTIES  _BACKTRACE_TRIPLES "F:/Documents/C++/MyProjects/Computer_room_booking_system/test/CMakeLists.txt;5;add_test;F:/Documents/C++/MyProjects/Computer_room_booking_system/test/CMakeLists.txt;0;")
